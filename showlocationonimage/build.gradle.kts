81a16e3d0acea1f4ca52f42b9018c4dfe8131bab
Aditi Patil <a@a.com>
2024-08-12 11:11:32 +0530

    Initial commit

---
+++ showlocationonimage/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    // gpt
+//    id("com.android.library")
+    id("kotlin-android")
+
+}
+
+android {
+    namespace = "com.pukkait.showlocationonimage"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.pukkait.showlocationonimage"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.8.20") // Adjust the version as needed
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
