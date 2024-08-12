05c81318cda027d14ce97c4245e5ab51273ee036
Aditi Patil <a@a.com>
2024-08-12 11:21:08 +0530

    cha

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.0.0") // Update to a compatible version
+        // Other classpath dependencies
+    }
+}
