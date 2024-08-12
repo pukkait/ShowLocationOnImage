ff2955f061b207517c5ef11f6aa70bddb3552fe1
Aditi Patil <a@a.com>
2024-08-12 16:20:29 +0530

    0.0.12

--- showlocationonimage/src/main/java/com/pukkait/showlocationonimage/TestToast.kt
+++ showlocationonimage/src/main/java/com/pukkait/showlocationonimage/TestToast.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 object TestToast {
+    @JvmStatic
     fun showToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
